a37462af61dfc333cc16c239484e039896c1c85d
LRaszka <Raszka.Lukas123@gmail.com>
2021-11-28 23:25:43 +0100

    Add project files.

---
+++ Hraci_Kostka_Objekty/Kostka.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hraci_Kostka_Objekty
+{
+    /// <summary>
+    /// Třída reprezentuje hrací kostku
+    /// </summary>
+    class Kostka
+    {
+        private Random random;
+        private int pocetSten;
+
+        public Kostka()
+        {
+            pocetSten = 6;
+            random = new Random();
+        }
+
+        public Kostka(int pocetSten)
+        {
+            this.pocetSten = pocetSten;
+            random = new Random();
+        }
+
+        public int VratPocetSten()
+        {
+            return pocetSten;
+        }
+
+        public int hod()
+        {
+            return random.Next(1, pocetSten + 1);
+        }
+
+        /// <summary>
+        /// Vrací textovou reprezentaci kostky
+        /// </summary>
+        /// <returns>Textová reprezentace kostky</returns>
+        public override string ToString()
+        {
+            return String.Format("Kostka s {0} stěnami", pocetSten);
+        }
+    }
+}
---
+++ Hraci_Kostka_Objekty/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hraci_Kostka_Objekty
+{
+    class Program
+    {
+        static void Main()
+        {
+            Kostka sestistenna = new Kostka(); // v tuto chvíli se zavolá konstruktor
+            Kostka desetistenna = new Kostka(10); // v tuto chvíli se zavolá konstruktor
+            Console.WriteLine(desetistenna.VratPocetSten());
+            Console.WriteLine(sestistenna.hod());
+            Console.WriteLine(sestistenna);
+            Console.ReadKey();
+        }
+    }
+}
